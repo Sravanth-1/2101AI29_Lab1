b62d4c003b625e283fce5db892e4beac4dffd8aa
Sravanth-1 <110086863+Sravanth-1@users.noreply.github.com>
2022-07-27 13:17:10 +0530

    Storage of operation to be done

--- code1.c
+++ code1.c
@@ -6,3 +6,4 @@ printf("Enter two numbers which are operands");
 scanf("%f%f",&a,&b);
 printf("Enter 0 for addition\nEnter 1 for subtraction\n");
 printf("Enter 2 for multiplication\nEnter 3 for division\n");
+scanf("%d",&c);
