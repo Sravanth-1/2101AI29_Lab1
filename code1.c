6e34b065381f673d3ad499f10873ddf6fa01ca02
Sravanth-1 <110086863+Sravanth-1@users.noreply.github.com>
2022-07-27 13:15:29 +0530

    Seeking input of Operation to be done

--- code1.c
+++ code1.c
@@ -4,3 +4,5 @@ float a,b;
 int c;
 printf("Enter two numbers which are operands");
 scanf("%f%f",&a,&b);
+printf("Enter 0 for addition\nEnter 1 for subtraction\n");
+printf("Enter 2 for multiplication\nEnter 3 for division\n");
